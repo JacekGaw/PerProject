b89e150213082beb6c153b1d256f082b04f36666
jacekgawlyta <jac.gawlyta@gmail.com>
2024-08-05 19:41:37 +0200

    Initial setup for backend

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"; 
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors()); 
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Connected to API");
+  });
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+});
