fc1d2180f55ce90adbabbc5622459249584c62f2
jacekgawlyta <jac.gawlyta@gmail.com>
2024-08-05 19:53:02 +0200

    Initial setup for the frontend part

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
