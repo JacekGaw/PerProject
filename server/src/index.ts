f2b406483682b47c8df89f2febc8fa6a93328485
jacekgawlyta <jac.gawlyta@gmail.com>
2024-08-09 22:42:01 +0200

    Define Dockerfiles, docker composeand nginx config, optimize for the smallest image sizes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors"; 
 import { runDB } from './database/db.js';
 runDB();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); 
 
 const PORT = process.env.PORT;
 
